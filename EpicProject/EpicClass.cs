4802bc82223a806b79c6b896aefdc8e57a4dd66f
Xevi8X <wggajda@gmail.com>
2023-02-27 19:45:25 +0100

    Dodaj pliki projektów.

---
+++ EpicProject/EpicClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicProject
+{
+    public class EpicClass
+    {
+        public static int ReturnOne()
+        {
+            return 1;
+        }
+    }
+}
